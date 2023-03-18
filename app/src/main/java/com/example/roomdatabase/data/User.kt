9496c2b72e346b6f053de2ce3c1da1c7e1e556b7
MaxS2021 <maxs2021git@gmail.com>
2023-03-18 22:19:13 +0300

    Room Database #1

---
+++ app/src/main/java/com/example/roomdatabase/data/User.kt
@@ -0,0 +1,12 @@
+package com.example.roomdatabase.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user_table")
+data class User(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val firsName: String,
+    val lastName: String,
+    val age: Int
+)
